0e386b916fda621ddae6d9f6b63a62242807c11d
Shoukat Ali <shoukatali488@gmail.com>
2019-11-17 23:02:30 +0500

    modified bst

--- Tree/BST/binarySearchTree.cpp
+++ Tree/BST/binarySearchTree.cpp
@@ -44,7 +44,7 @@ class TreeNode{
         if(leftNode != NULL)
         leftNode->traverseInOrder();
 
-        cout<<data<<" ";
+        // cout<<data<<" ";
 
         if(rightNode != NULL)
         rightNode->traverseInOrder();
@@ -79,14 +79,17 @@ int main()
 {
 
     Tree *tree = new Tree();
-
-    tree->insert(3);
+//5,4,8,6,7,10,2,18,14,15
     tree->insert(5);
-    tree->insert(20);
-    tree->insert(1);
-    tree->insert(7);
     tree->insert(4);
+    tree->insert(8);
+    tree->insert(6);
+    tree->insert(7);
     tree->insert(10);
+    tree->insert(2);
+    tree->insert(18);
+    tree->insert(14);
+    tree->insert(15);
 
     tree->traverseInOrder();
 }
